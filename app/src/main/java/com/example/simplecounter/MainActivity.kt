e6e9da18ec087b67ce48638bdd09ff7cd6adf9e8
Nilemos <you@example.com>
2024-10-23 16:26:07 +0300

    add sharedPreferences

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.simplecounter
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -7,7 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
+
 
 class MainActivity : AppCompatActivity() {
     private var counter = 0
@@ -15,10 +17,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonIncrement: Button
     private lateinit var buttonDecrement: Button
     private lateinit var buttonRes: Button
+    private lateinit var preferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        preferences = getPreferences(Context.MODE_PRIVATE)
+        counter = preferences.getInt("key", 0)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -28,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         buttonIncrement = findViewById<Button>(R.id.button2)
         buttonDecrement = findViewById<Button>(R.id.button)
         buttonRes = findViewById<Button>(R.id.button3)
-
+        textViewCounter.text = counter.toString()
         buttonIncrement.setOnClickListener {
             counter++
             textViewCounter.text = counter.toString()
@@ -48,5 +53,10 @@ class MainActivity : AppCompatActivity() {
             counter = 0
             textViewCounter.text = counter.toString()
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferences.edit().putInt("key", counter).apply()
     }
 }
